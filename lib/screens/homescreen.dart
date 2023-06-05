e90353403a643b61d9db95116907b95ef493ff02
Ahmad Maaz <ahmad.maaz273@gmail.com>
2023-06-05 23:06:04 +0500

    working on weather Api

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -6,15 +6,34 @@ import '../widgets/night_card.dart';
 import '../widgets/night_minicard.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  final locationWeather;
+  const HomeScreen({Key? key, this.locationWeather}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    updateUI(widget.locationWeather);
+  }
+
   // PageController _pageController = PageController(initialPage: 0);
   bool isSecondOrder = false;
+  double? temperature;
+  int? condition;
+  String? cityName;
+
+  void updateUI(dynamic weatherData) {
+    temperature = weatherData['main']['temp'];
+    condition = weatherData['weather'][0]['id'];
+    cityName = weatherData['name'];
+
+    print(temperature);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -93,3 +112,7 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
+      // var temperature = decodedData['main']['temp'];
+      // var condition = decodedData['weather'][0]['id'];
+      // var cityName = decodedData['name'];
--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/screens/homescreen.dart';
+import 'package:weather_app/services/networking.dart';
 
 import '../services/location.dart';
+
+const apiKey = '0c324e13be8028353f5765fc15489f22';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -11,38 +14,49 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  dynamic latitude;
+  dynamic longitude;
+
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+    latitude = location.latitude;
+    longitude = location.longitude;
 
-    Future.delayed(const Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const HomeScreen(),
+    var url = Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric');
+    NetworkHelper networkHelper = NetworkHelper(url);
+
+    var weatherData = await networkHelper.getData();
+    // ignore: avoid_print
+    // print(weatherData);
+
+    // Future.delayed(const Duration(seconds: 3), () {
+    // ignore: use_build_context_synchronously
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => HomeScreen(
+          locationWeather: weatherData,
         ),
-      );
-    });
+      ),
+    );
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor : const Color.fromARGB(255, 0, 65, 134).withOpacity(0.5),
+      backgroundColor: const Color.fromARGB(255, 0, 65, 134).withOpacity(0.5),
       body: const Center(
-        child: 
-            CircularProgressIndicator(color: Colors.white),
-          
-        ),
-      
+        child: CircularProgressIndicator(color: Colors.white),
+      ),
     );
   }
 }
